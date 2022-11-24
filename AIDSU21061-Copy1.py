3b3b058a1c9939846301414f5492475ec1881ac1
mrknox123 <92286857+mrknox123@users.noreply.github.com>
2022-11-24 12:33:14 +0530

    py

---
+++ AIDSU21061-Copy1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # PRACTICAL NO.3
+# 
+# 
+
+# In[ ]:
+
+
+# Program using Arithmatic and logical operation 
+
+
+# In[24]:
+
+
+a=int(input("Enter a : "))
+b=int(input("Enter b : "))
+
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+h=a//b
+
+print("Addition of a & b",c)
+print("Substraction of a & b",d)
+print("multiplication of a & b",e)
+print("division of a & b",f)
+print("module of a & b",g)
+print("foor division of a & b",h)
+
+
+# In[ ]:
+
+
+
+
